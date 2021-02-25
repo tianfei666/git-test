f0db83af59d76732f9ab8d4324d719219bbfa68e
tianfei <513581949@qq.com>
2021-02-25 17:31:23 +0800

    132

---
+++ git_test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,22 @@
+package com.itheima.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author fei
+ * @create 2021--02--25--16:39
+ */
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("hellow git project");
+		System.out.println("hellow git project");
+		System.out.println("hellow git project");
+
+		System.out.println("hellow git project");
+		System.out.println("hellow git project");
+		System.out.println("hellow git project");
+
+	}
+
+}
